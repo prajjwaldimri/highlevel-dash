3f7a2f19ce0c042bd5f487ca7518e7c9ced0fee1
Prajjwal <prajjwaldimri@hotmail.com>
2019-04-18 01:11:12 +0530

    Added css folder to public

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Nav></Nav>
+  </div>
 </template>
 
 <script>
+import Nav from "./components/Nav.vue";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    Nav
+  }
 };
 </script>
